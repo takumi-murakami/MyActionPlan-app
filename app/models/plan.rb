751ea75edac4f6c49a5c477e6bf0c223a19d953c
takumi-murakami <takumi28280503@gmail.com>
2020-10-22 18:42:16 +0900

    cssの修正

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -1,4 +1,15 @@
 class Plan < ApplicationRecord
   belongs_to :user
   has_one :address
+  has_one_attached :image
+  # belongs_to_active_hash :genre
+  # belongs_to_active_hash :prefecture
+
+  with_options presence: true do
+    validates :title, :text, :image, :action_date
+  
+    # with_options numericality: { other_than: 1 } do
+    #   validates :genre_id, :prefecture_id
+    # end
+  end
 end
